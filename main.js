7613c6c9f368650483b82db828916d7ad82d48c5
TibiaBones <78929148+TibiaBones@users.noreply.github.com>
2026-08-23 18:23:47 +0300

    add a script file, add three arrays with product information

---
+++ main.js
@@ -0,0 +1,215 @@
+const bestSellerProducts = [
+    {
+        id: 1,
+        name: 'Nutrabey Gold 100% Whey Protein Concentrate',
+        currentPrice: 2419.00,      
+        originalPrice: 2419.00,     
+        discount: '-50%',           
+        imgSrc: 'img/shopImg/PasteImage1.png',
+        rating: 5
+    },
+
+    {
+        id: 2,
+        name: 'Nutrabey Gold 100% Whey Protein Concentrate',
+        currentPrice: 2419.00,      
+        originalPrice: 2419.00,     
+        discount: '-50%',           
+        imgSrc: 'img/shopImg/PasteImage1.png',
+        rating: 5
+    },
+
+    {
+        id: 3,
+        name: 'Nutrabey Gold 100% Whey Protein Concentrate',
+        currentPrice: 2419.00,      
+        originalPrice: 2419.00,     
+        discount: '-50%',           
+        imgSrc: 'img/shopImg/PasteImage1.png',
+        rating: 5
+    },
+
+    {
+        id: 4,
+        name: 'Nutrabey Gold 100% Whey Protein Concentrate',
+        currentPrice: 2419.00,      
+        originalPrice: 2419.00,     
+        discount: '-50%',           
+        imgSrc: 'img/shopImg/PasteImage1.png',
+        rating: 5
+    },
+
+    {
+        id: 5,
+        name: 'Nutrabey Gold 100% Whey Protein Concentrate',
+        currentPrice: 2419.00,      
+        originalPrice: 2419.00,     
+        discount: '-50%',           
+        imgSrc: 'img/shopImg/PasteImage1.png',
+        rating: 5
+    },
+
+    {
+        id: 6,
+        name: 'Nutrabey Gold 100% Whey Protein Concentrate',
+        currentPrice: 2419.00,      
+        originalPrice: 2419.00,     
+        discount: '-50%',           
+        imgSrc: 'img/shopImg/PasteImage1.png',
+        rating: 5
+    },
+
+    {
+        id: 7,
+        name: 'Nutrabey Gold 100% Whey Protein Concentrate',
+        currentPrice: 2419.00,      
+        originalPrice: 2419.00,     
+        discount: '-50%',           
+        imgSrc: 'img/shopImg/PasteImage1.png',
+        rating: 5
+    },
+]
+
+const supplementsProducts = [
+    {
+        id: 1,
+        name: 'Nutrabey Gold 100% Whey Protein Concentrate',
+        currentPrice: 2419.00,      
+        originalPrice: 2419.00,     
+        discount: '-50%',           
+        imgSrc: 'img/shopImg/PasteImage5.png',
+        rating: 5
+    },
+
+    {
+        id: 2,
+        name: 'Nutrabey Gold 100% Whey Protein Concentrate',
+        currentPrice: 2419.00,      
+        originalPrice: 2419.00,     
+        discount: '-50%',           
+        imgSrc: 'img/shopImg/PasteImage5.png',
+        rating: 5
+    },
+
+    {
+        id: 3,
+        name: 'Nutrabey Gold 100% Whey Protein Concentrate',
+        currentPrice: 2419.00,      
+        originalPrice: 2419.00,     
+        discount: '-50%',           
+        imgSrc: 'img/shopImg/PasteImage5.png',
+        rating: 5
+    },
+
+    {
+        id: 4,
+        name: 'Nutrabey Gold 100% Whey Protein Concentrate',
+        currentPrice: 2419.00,      
+        originalPrice: 2419.00,     
+        discount: '-50%',           
+        imgSrc: 'img/shopImg/PasteImage5.png',
+        rating: 5
+    },
+
+    {
+        id: 5,
+        name: 'Nutrabey Gold 100% Whey Protein Concentrate',
+        currentPrice: 2419.00,      
+        originalPrice: 2419.00,     
+        discount: '-50%',           
+        imgSrc: 'img/shopImg/PasteImage5.png',
+        rating: 5
+    },
+
+    {
+        id: 6,
+        name: 'Nutrabey Gold 100% Whey Protein Concentrate',
+        currentPrice: 2419.00,      
+        originalPrice: 2419.00,     
+        discount: '-50%',           
+        imgSrc: 'img/shopImg/PasteImage5.png',
+        rating: 5
+    },
+
+    {
+        id: 7,
+        name: 'Nutrabey Gold 100% Whey Protein Concentrate',
+        currentPrice: 2419.00,      
+        originalPrice: 2419.00,     
+        discount: '-50%',           
+        imgSrc: 'img/shopImg/PasteImage5.png',
+        rating: 5
+    },
+]
+
+const newLaunchedProducts = [
+    {
+        id: 1,
+        name: 'Nutrabey Gold 100% Whey Protein Concentrate',
+        currentPrice: 2419.00,      
+        originalPrice: 2419.00,     
+        discount: '-50%',           
+        imgSrc: 'img/shopImg/PasteImage7.png',
+        rating: 5
+    },
+
+    {
+        id: 2,
+        name: 'Nutrabey Gold 100% Whey Protein Concentrate',
+        currentPrice: 2419.00,      
+        originalPrice: 2419.00,     
+        discount: '-50%',           
+        imgSrc: 'img/shopImg/PasteImage7.png',
+        rating: 5
+    },
+
+    {
+        id: 3,
+        name: 'Nutrabey Gold 100% Whey Protein Concentrate',
+        currentPrice: 2419.00,      
+        originalPrice: 2419.00,     
+        discount: '-50%',           
+        imgSrc: 'img/shopImg/PasteImage7.png',
+        rating: 5
+    },
+
+    {
+        id: 4,
+        name: 'Nutrabey Gold 100% Whey Protein Concentrate',
+        currentPrice: 2419.00,      
+        originalPrice: 2419.00,     
+        discount: '-50%',           
+        imgSrc: 'img/shopImg/PasteImage7.png',
+        rating: 5
+    },
+
+    {
+        id: 5,
+        name: 'Nutrabey Gold 100% Whey Protein Concentrate',
+        currentPrice: 2419.00,      
+        originalPrice: 2419.00,     
+        discount: '-50%',           
+        imgSrc: 'img/shopImg/PasteImage7.png',
+        rating: 5
+    },
+
+    {
+        id: 6,
+        name: 'Nutrabey Gold 100% Whey Protein Concentrate',
+        currentPrice: 2419.00,      
+        originalPrice: 2419.00,     
+        discount: '-50%',           
+        imgSrc: 'img/shopImg/PasteImage7.png',
+        rating: 5
+    },
+
+    {
+        id: 7,
+        name: 'Nutrabey Gold 100% Whey Protein Concentrate',
+        currentPrice: 2419.00,      
+        originalPrice: 2419.00,     
+        discount: '-50%',           
+        imgSrc: 'img/shopImg/PasteImage7.png',
+        rating: 5
+    },
+]
